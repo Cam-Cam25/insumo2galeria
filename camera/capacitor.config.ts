d1338f117c1020d92dd901bf70d46002c984072f
camcamguaman25 <camcamguaman25@gmail.com>
2025-03-05 20:01:27 +0000

    second

--- camera/capacitor.config.ts
+++ camera/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'myapp',
-  webDir: 'dist'
+  webDir: 'dist/myapp/browser',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
